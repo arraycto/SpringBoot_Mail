9c13093c087fe2d148dee726bc52f2496ad254a8
23369 <123456>
2020-01-20 22:07:08 +0800

    简单文本邮件、HTML邮件、附件邮件、多个附件邮件、图片附件、邮件模板

--- src/main/java/com/hern/service/MailService.java
+++ src/main/java/com/hern/service/MailService.java
@@ -4,10 +4,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.util.ArrayList;
 
 /**
  * @program: springboot-mail
@@ -32,6 +40,7 @@ public class MailService {
     }
 
     /*
+    * 发送简单文本邮件
     * to：发送给谁
     * subject：发送的主题（邮件主题）
     * content：发送的内容
@@ -52,4 +61,123 @@ public class MailService {
         }
     }
 
+    /*
+     * 发送HTML邮件
+     * to：发送给谁
+     * subject：发送的主题（邮件主题）
+     * content：发送的内容
+     * */
+    public void sendHtmlMail(String to, String subject, String content) throws MessagingException {
+
+        MimeMessage mimeMailMessage = mailSender.createMimeMessage();
+
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMailMessage,true);
+        mimeMessageHelper.setTo(to);
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setText(content,true);
+        mimeMessageHelper.setFrom(from);
+
+        try {
+            mailSender.send(mimeMailMessage);
+            logger.info("HTML邮件发送成功！");
+        } catch (MailException e) {
+            logger.error("HTML邮件发送失败！",e);
+        }
+    }
+
+    /*
+     * 发送附件邮件
+     * to：发送给谁
+     * subject：发送的主题（邮件主题）
+     * content：发送的内容
+     * filePath：附件路径
+     * */
+    public void sendAttachmentsMail(String to, String subject, String content, String filePath) throws MessagingException {
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,true);
+        mimeMessageHelper.setTo(to);
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setText(content);
+
+        //获取文件路径以及文件名称
+        FileSystemResource fileSystemResource = new FileSystemResource(new File(filePath));
+        String fileName = fileSystemResource.getFilename();
+
+        mimeMessageHelper.addAttachment(fileName,fileSystemResource);
+
+        mimeMessageHelper.setFrom(from);
+
+        try {
+            mailSender.send(mimeMessage);
+            logger.info("附件邮件发送成功！");
+        } catch (MailException e) {
+            logger.error("附件邮件发送失败！",e);
+        }
+    }
+
+    /*
+     * 发送多个附件邮件
+     * to：发送给谁
+     * subject：发送的主题（邮件主题）
+     * content：发送的内容
+     * filePath：附件路径
+     * */
+    public void sendAttachmentsMails(String to, String subject, String content, ArrayList filePaths) throws MessagingException {
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,true);
+        mimeMessageHelper.setTo(to);
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setText(content);
+
+        //获取文件路径以及文件名称
+
+        for (Object filepath:filePaths) {
+            FileSystemResource fileSystemResource = new FileSystemResource(new File((String) filepath));
+            String fileName = fileSystemResource.getFilename();
+
+            mimeMessageHelper.addAttachment(fileName,fileSystemResource);
+        }
+        mimeMessageHelper.setFrom(from);
+
+        try {
+            mailSender.send(mimeMessage);
+            logger.info("附件邮件发送成功！");
+        } catch (MailException e) {
+            logger.error("附件邮件发送失败！",e);
+        }
+    }
+
+    /*
+     * 发送图片件
+     * to：发送给谁
+     * subject：发送的主题（邮件主题）
+     * content：发送的内容
+     * imagePath：图片路径
+     * imageId：图片ID
+     * */
+    public void sendImage(String to, String subject, String content, String imagePath, String imageId) throws MessagingException {
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,true);
+        mimeMessageHelper.setTo(to);
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setText(content,true);
+
+        FileSystemResource fileSystemResource = new FileSystemResource(new File(imagePath));
+
+        mimeMessageHelper.addInline(imageId,fileSystemResource);
+        mimeMessageHelper.setFrom(from);
+
+        try {
+            logger.info("图片邮件发送成功!");
+            mailSender.send(mimeMessage);
+        } catch (MailException e) {
+            logger.error("图片邮件发送失败！",e);
+        }
+    }
 }
--- src/main/java/com/hern/service/MailServiceTest.java
+++ src/main/java/com/hern/service/MailServiceTest.java
@@ -4,8 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.annotation.Resource;
+import javax.mail.MessagingException;
+import java.util.ArrayList;
 
 /**
  * @program: springboot-mail
@@ -21,8 +25,64 @@ public class MailServiceTest {
     @Resource
     MailService mailService;
 
+    @Resource
+    TemplateEngine templateEngine;
+
+    //测试简单文本文件
     @Test
     public void sendSimpleMail(){
         mailService.sendSimpleMail("13176567194@163.com","Springboot-Mail测试","基于SpringBoot的邮件系统的文本邮件");
+    }
+
+    //发送HTML邮件
+    @Test
+    public void sendHtmlMail() throws MessagingException {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<html>\n").append("<body>\n").append("<h1>HTML邮件系统测试</h1>\n").append("</body>\n").append("</html>\n");
+        //String content = "<html>\n"+"<body>\n"+"<h1>HTML邮件系统测试</h1>\n"+"</body>\n"+"</html>\n";
+        mailService.sendHtmlMail("13176567194@163.com","HTML邮件测试", String.valueOf(stringBuilder));
+    }
+
+    //发送单个附件邮件
+    @Test
+    public void sendAttachmentsMail() throws MessagingException {
+
+        String filePath = "src/main/resources/templates/2019.12杨寨工资明细.xls";
+
+        mailService.sendAttachmentsMail("13176567194@163.com","多个附件邮件测试","附件邮件",filePath);
+    }
+
+    //发送多个附件邮件
+    @Test
+    public void sendAttachmentsMails() throws MessagingException {
+
+        String filePath = "src/main/resources/templates/2019.12杨寨工资明细.xls";
+        String filePath2 = "src/main/resources/templates/2019.12杨寨工资明细2.xls";
+
+        ArrayList<String> filePaths = new ArrayList();
+        filePaths.add(filePath);
+        filePaths.add(filePath2);
+
+        mailService.sendAttachmentsMails("13176567194@163.com","多个附件邮件测试","附件邮件",filePaths);
+    }
+
+    @Test
+    public void sendImage() throws MessagingException {
+
+        String imagePath = "src/main/resources/templates/ZoomOut_ZH-CN4471982075_1920x1080.jpg";
+        String imageId = "123";
+        StringBuilder content = new StringBuilder();
+        content.append("<html>\n").append("<body>\n").append("<img src=\"").append(imagePath).append("\">\n").append("</body>\n").append("</html>");
+        mailService.sendImage("13176567194@163.com","图片邮件","图片邮件",imagePath,imageId);
+
+    }
+
+    @Test
+    public void templateMail() throws MessagingException {
+        Context context = new Context();
+        context.setVariable("id","123");
+
+        String emailContent = templateEngine.process("emailTemplate",context);
+        mailService.sendHtmlMail("13176567194@163.com","模板邮件",emailContent);
     }
 }
